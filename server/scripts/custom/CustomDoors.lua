7a725a6c67e696c2ae00933ba5a678934bbe6f4a
Skooma-Breath <110526221+Skooma-Breath@users.noreply.github.com>
2023-04-19 23:51:57 -0500

    Update CustomDoors.lua

--- server/scripts/custom/CustomDoors.lua
+++ server/scripts/custom/CustomDoors.lua
@@ -2,6 +2,13 @@
 --1) Save this file as "CustomDoors.lua" in server/scripts/custom
 --2) Add   require("custom.CustomDoors")   to customScripts.lua
 
+--how to do stuff
+
+-- use /placeat selection_tool to spawn the tool. 
+-- hit a door to select it after typing /copy then go to where you want the destination to be and type /setdoor.
+-- /paste will place a copy of whatever object you have selected in front of you. (best to have a decorate script to use this)
+-- /removedoor will remove a door that you have re routed but players that have used that door since they logged in will have to relog
+-- to clear the destination for them.(figure out if this can be fixed)
 
 local CustomDoors = {}
 
