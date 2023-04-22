0b63e5a8d6a88f8191ad18150ba762dcfafd5ba4
Skooma-Breath <110526221+Skooma-Breath@users.noreply.github.com>
2023-04-22 06:00:57 -0500

    Update CustomDoors.lua

--- server/scripts/custom/CustomDoors.lua
+++ server/scripts/custom/CustomDoors.lua
@@ -7,7 +7,7 @@
 -- use /placeat selection_tool to spawn the tool. 
 -- hit a door to select it after typing /copy then go to where you want the destination to be and type /setdoor.
 -- /paste will place a copy of whatever object you have selected in front of you. (best to have a decorate script to use this)
--- /removedoor will remove a door that you have re routed but players that have used that door since they logged in will have to relog
+-- /removedoor will remove the custom destination of the door you have selected, but players that have used that door since they logged in will have to relog
 -- to clear the destination for them.(figure out if this can be fixed)
 -- /realmofdoors will teleport you to the realm of doors
 
